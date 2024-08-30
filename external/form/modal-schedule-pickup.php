dee0c05dc61d6425e2528dc7aa958b01a47c9de5
25Dark <henrydanso6@gmail.com>
2024-08-30 02:40:54 +0000

    buggy buggy

--- external/form/modal-schedule-pickup.php
+++ external/form/modal-schedule-pickup.php
@@ -18,7 +18,7 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'Premium Bubble';                     //SMTP username
+    $mail->Username   = 'smartbeeinc6@gmail.com';                     //SMTP username
     $mail->Password   = 'ffxx yjmw fuwv klst';                               //SMTP password
     $mail->SMTPSecure = "ssl";            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
