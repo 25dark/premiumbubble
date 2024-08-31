1a15d512d7b6f04cfd9b0666fc334851d0652502
25Dark <henrydanso6@gmail.com>
2024-08-31 03:46:19 +0000

    navbar fixed

--- external/form/modal-schedule-pickup.php
+++ external/form/modal-schedule-pickup.php
@@ -17,11 +17,9 @@ try {
     // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    // $mail->Host       = 'smtp.titan.email';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'smartbeeinc6@gmail.com';                     //SMTP username
     $mail->Password   = 'hhir pkcf qkgm gsit';                               //SMTP password
-    // $mail->SMTPSecure = "ssl";            //Enable implicit TLS encryption
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     // $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
