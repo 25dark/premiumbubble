ebdea196bed737c937a6cf04af4d29e0373bca2b
25Dark <henrydanso6@gmail.com>
2024-09-02 02:44:48 +0000

    newsletter updated

--- external/form/newsletter-form.php
+++ external/form/newsletter-form.php
@@ -14,31 +14,31 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'mailwebsmirno@gmail.com';                     //SMTP username
-    $mail->Password   = 'mail0web_smirnoQ';                               //SMTP password
-    $mail->SMTPSecure = "ssl";            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Username   = 'smartbeeinc6@gmail.com';                     //SMTP username
+    $mail->Password   = 'hhir pkcf qkgm gsit';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    //Recipients
-    $mail->setFrom('mailwebsmirno@gmail.com', 'ProLaundry');
-    $mail->addAddress('youremail@gmail.com', 'ProLaundry');     //Add a recipient
+     // Recipients
+$mail->setFrom('smartbeeinc6@gmail.com', 'Premium Bubble');
+$mail->addAddress('smartbeeinc6@gmail.com', 'Premium Bubble Laundry Pick-up'); // Add a recipient
 
     //Passed variables
     $email = $_POST['email'];
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is what was sent ProLaundry, form "NEWSLETTER SIGNUP"';
+    $mail->Subject = 'Here is what was sent Premium Bubble, form "NEWSLETTER SIGNUP"';
     $mail->Body    =
     	'E-mail: ' .$email;
     //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
-    echo 'Message has been sent';
+    header('Location: ../../messagesent.html');
+    exit();
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
